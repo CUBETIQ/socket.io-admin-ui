afb8fc343826c68ccdab9dca190d0afba8b2771c
Sambo Chea <sombochea@cubetiqs.com>
2021-09-06 16:01:25 +0700

    Add force remove docker container when run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,10 @@
 #!/bin/sh -e
 
 IMAGE=registry.kh.cubetiqs.com/socketio-admin-ui:latest
+NAME=socketio-admin-ui
 
 docker pull "${IMAGE}"
 
-docker run -d -p 8015:80 --restart always --name socketio-admin-ui "${IMAGE}"
+docker rm -f "${NAME}"
+
+docker run -d -p 8015:80 --restart always --name "${NAME}" "${IMAGE}"
