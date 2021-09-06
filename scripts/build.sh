b2e4c8fffa62d79b11ab05653279ce75a860f707
Sambo Chea <sombochea@cubetiqs.com>
2021-09-06 15:46:33 +0700

    Add custom script and docekrfile for build the admin ui

---
+++ scripts/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+
+SCRIPT_DIR=$(dirname "$0")
+
+REGISTRY=registry.kh.cubetiqs.com
+IMAGE=socket.io-admin
+TAG=latest
+HUB_NAME="${REGISTRY}/${IMAGE}:${TAG}"
+DOCKERFILE_PATH=${SCRIPT_DIR}/ui/Dockerfile.prod
+
+docker build . -f "${DOCKERFILE_PATH}" -t "${HUB_NAME}"
+
+docker push "${HUB_NAME}"
